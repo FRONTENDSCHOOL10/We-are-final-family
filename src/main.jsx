bf4ce899d39923346e3c9a8e149b3c2cd7025d92
dongho <ldd0702@naver.com>
2024-08-29 17:19:48 +0900

    init 리셋 컬러토큰

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "@/styles/globals.css";
-import App from "/App";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@/styles/main.css';
+import App from '/App';
 
-const domNode = document.getElementById("react-app");
+const domNode = document.getElementById('react-app');
 
 createRoot(domNode).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
