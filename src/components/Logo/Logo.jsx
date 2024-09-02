c1a76b65f44f26ed848ea234891e209f37fb6e49
dongho <ldd0702@naver.com>
2024-09-02 21:19:47 +0900

    Feature 인트로 페이지 마크업 스타일링
    - Logo 컴포넌트

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+function Logo({ W = '', H = '' }) {
+  // width와 height가 숫자로 전달된 경우 단위를 붙입니다.
+  const widthUnit = typeof width === 'number' ? `${W}` : W;
+  const heightUnit = typeof height === 'number' ? `${H}` : H;
+
+  return (
+    <img
+      src="/logo.svg"
+      alt="logo"
+      style={{ width: widthUnit, height: heightUnit }}
+    />
+  );
+}
+
+export default Logo;
