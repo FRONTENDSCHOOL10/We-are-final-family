4e5a36da612412e8283341eb95c5e62484173163
dongho <ldd0702@naver.com>
2024-09-02 21:36:40 +0900

    Feature 카테고리 필터 버튼 컴포넌트
    - 카테고리 필터 버튼 컴포넌트 마크업 스타일링 기능구현
    - 필터 버튼 데이터 추가
    - 카테고리 셀렉트 데이터 수정
    - 카테고리 셀렉트 수정

--- src/components/CategorySelect/CategorySelect.jsx
+++ src/components/CategorySelect/CategorySelect.jsx
@@ -1,60 +1,27 @@
 import { useState } from 'react';
 import S from './CategorySelect.module.css';
+import { CategoryOptions } from './data/CategorySelectData';
 
-export default function CategorySelect() {
+function CategorySelect() {
   const [selectedOption, setSelectedOption] = useState('');
   const [isOpen, setIsOpen] = useState(false);
 
-  // 옵션 데이터 배열
-  const options = [
-    { value: '', label: '카테고리를 선택해주세요.', disabled: true },
-    { value: 'frontend', label: '프론트엔드' },
-    { value: 'backend', label: '백엔드' },
-    { value: 'fullstack', label: '풀스택' },
-    { value: 'algorithm', label: '알고리즘' },
-    { value: 'basics', label: '기초지식' },
-    { value: 'react', label: '리액트' },
-    { value: 'uidesign', label: 'UI 디자인' },
-    { value: 'uxdesign', label: 'UX 디자인' },
-    { value: 'uiux', label: 'UI/UX' },
-    { value: 'dataanalysis', label: '데이터분석' },
-    { value: 'statistics', label: '통계분석' },
-    { value: 'visualization', label: '시각화' },
-  ];
-
-  const handleChange = (event) => {
-    setSelectedOption(event.target.value);
+  function handleChange(value) {
+    setSelectedOption(value);
     setIsOpen(false);
-  };
+  }
 
-  const toggleDropdown = () => {
+  function toggleDropdown() {
     setIsOpen(!isOpen);
-  };
+  }
 
   return (
     <div className={S.con}>
       <div className={S.selectWrapper} onClick={toggleDropdown}>
-        <select
-          value={selectedOption}
-          onChange={handleChange}
-          className={S.sel}
-          style={{ display: 'none' }}
-        >
-          {options.map((option) => (
-            <option
-              key={option.value}
-              value={option.value}
-              disabled={option.disabled}
-              className={S.opt}
-            >
-              {option.label}
-            </option>
-          ))}
-        </select>
         <div className={S.selectedOption}>
           {selectedOption
-            ? options.find((opt) => opt.value === selectedOption)?.label
-            : '카테고리를 선택해주세요.'}
+            ? CategoryOptions.find((opt) => opt.value === selectedOption)?.label
+            : '카테고리를 선택해주세요'}
         </div>
         <i
           className={`${isOpen ? 'i_direction_top' : 'i_direction_bottom'} ${
@@ -64,16 +31,11 @@ export default function CategorySelect() {
       </div>
       {isOpen && (
         <ul className={S.optionList}>
-          {options.map((option) => (
+          {CategoryOptions.map((option) => (
             <li
               key={option.value}
               className={S.optionItem}
-              onClick={() => {
-                if (!option.disabled) {
-                  setSelectedOption(option.value);
-                  setIsOpen(false);
-                }
-              }}
+              onClick={() => handleChange(option.value)}
             >
               {option.label}
             </li>
@@ -83,3 +45,5 @@ export default function CategorySelect() {
     </div>
   );
 }
+
+export default CategorySelect;
---
+++ src/components/CategorySelect/data/CategorySelectData.js
@@ -0,0 +1,14 @@
+export const CategoryOptions = [
+  { value: 'frontend', label: '프론트엔드', icon: 'i_home_line' },
+  { value: 'backend', label: '백엔드', icon: 'i_board_line' },
+  { value: 'fullstack', label: '풀스택', icon: 'i_calendar_line' },
+  { value: 'algorithm', label: '알고리즘', icon: 'i_chat_line' },
+  { value: 'basics', label: '기초지식', icon: 'i_comment_line' },
+  { value: 'react', label: '리액트', icon: 'i_location_line' },
+  { value: 'uidesign', label: 'UI 디자인', icon: 'i_people_line' },
+  { value: 'uxdesign', label: 'UX 디자인', icon: 'i_picture_line' },
+  { value: 'uiux', label: 'UI/UX', icon: 'i_profile_line' },
+  { value: 'dataanalysis', label: '데이터분석', icon: 'i_search' },
+  { value: 'statistics', label: '통계분석', icon: 'i_target' },
+  { value: 'visualization', label: '시각화', icon: 'i_export' },
+];
