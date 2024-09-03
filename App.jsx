5db98230890168f9bf397cbf6d5d9f1abff3f9f9
Gomyeunghan <rhaudgks04@gmail.com>
2024-09-03 11:01:34 +0900

    Feat: 말풍선 컴포넌트 및 현재시간 컴포넌트 제작
    - prop으로 내채팅,상대방채팅, userId 받음
    userId 일치하지 않을경우 상대방채팅으로 인식하고 오른쪽으로 띄우고 스타일 달라야함

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
+import { ChatSpeechbubble } from '@/components/ChatSpeechbubble/ChatSpeechbubble';
 import '@/styles/iconfonts.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React 웹 앱</h1>
+      <ChatSpeechbubble />
     </div>
   );
 }
