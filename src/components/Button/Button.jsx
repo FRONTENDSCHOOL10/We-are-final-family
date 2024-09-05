af2c19da57322d84d2d2b46a1fe6f50be0691bae
Gomyeunghan <rhaudgks04@gmail.com>
2024-09-05 16:13:42 +0900

    Refactor: 버튼 컴포넌트 to 속성 들어오면 LInk 태그로 변함
    -해당 프롭 문자값으로 경로전달

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import S from './Button.module.css';
 import { node, string, func, bool } from 'prop-types';
+import { Link } from 'react-router-dom';
 
 Button.propTypes = {
   onClick: func,
@@ -9,10 +10,12 @@ Button.propTypes = {
   children: node,
   color: string.isRequired,
   type: string,
+  to: string,
 };
 //porps color 로 [black,primary,white] 전달하면 스타일 입혀짐 문자로 전달해주세요
 // disabled 값 true or flase
 // type 값 string 필수로 입력
+// to 속성 들어오면 link 태그로 변함
 
 function Button({
   onClick,
@@ -20,13 +23,14 @@ function Button({
   children,
   color,
   type = 'button',
+  to,
   ...props
 }) {
   const variantClass = S[color];
 
   const buttonClassName = clsx(S.button, variantClass);
 
-  return (
+  return !to ? (
     <button
       type={type}
       onClick={onClick}
@@ -36,6 +40,19 @@ function Button({
     >
       {children}
     </button>
+  ) : (
+    <Link
+      style={{
+        textDecoration: 'none',
+      }}
+      to={to}
+      onClick={onClick}
+      disabled={disabled}
+      className={`${buttonClassName} hdg-md`}
+      {...props}
+    >
+      {children}
+    </Link>
   );
 }
 
