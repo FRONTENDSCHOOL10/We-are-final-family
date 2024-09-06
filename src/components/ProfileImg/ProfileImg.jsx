d730ee7761d8a0f862e038fc4675607d6386912d
Gomyeunghan <rhaudgks04@gmail.com>
2024-09-06 16:17:46 +0900

    Feat: UserCard 컴포넌트 기능 추가
    -  Prop 으로 'profile' 넣으면 내프로필 카드로 출력됨

--- src/components/ProfileImg/ProfileImg.jsx
+++ src/components/ProfileImg/ProfileImg.jsx
@@ -1,22 +1,36 @@
-import { string } from 'prop-types';
+import { string, func } from 'prop-types';
 import S from './ProfileImg.module.css';
 
 ProfileImg.propTypes = {
   image: string,
   width: string,
   height: string,
+  display: string,
+  onClick: func,
 };
 
 export function ProfileImg({
   image = '/src/assets/testImg/bonobobono.jpeg',
   width = '',
   height = '',
+  display = 'none',
+  onClick,
 }) {
   const styled = {
     backgroundImage: `url(${image})`,
-    width: width,
-    height: height,
+    minWidth: width,
+    minHeight: height,
   };
 
-  return <div className={S.component} style={styled}></div>;
+  return (
+    <div>
+      <div className={S.component} style={styled}>
+        <span
+          className={`${S.pencil} i_pencil`}
+          style={{ display: `${display}` }}
+          onClick={onClick}
+        ></span>
+      </div>
+    </div>
+  );
 }
--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,8 +1,8 @@
 import S from './UserCard.module.css';
-import { node, string } from 'prop-types';
+import { node, string, number } from 'prop-types';
 import { ProfileTitle } from '../ProfileTitle/ProfileTitle';
 import { ProfileImg } from '../ProfileImg/ProfileImg';
-import { useState, number } from 'react';
+import { useState } from 'react';
 
 UserCard.propTypes = {
   children: node,
@@ -33,6 +33,7 @@ export function UserCard({ description = '연남동', userId, states }) {
       return (
         <div className={S.wrapper}>
           <ProfileImg></ProfileImg>
+          <span className=""></span>
           <div className={S.component}>
             <ProfileTitle name="고명한" className={'lbl-md'}>
               {userId === userId ? (
@@ -46,6 +47,29 @@ export function UserCard({ description = '연남동', userId, states }) {
             </ProfileTitle>
             <p className="para-sm" style={{ color: 'var(--gray-600)' }}>
               {description}
+            </p>
+          </div>
+        </div>
+      );
+    case 'profile':
+      return (
+        <div className={S.profile}>
+          <ProfileImg
+            width={'4.84375rem'}
+            height={'4.84375rem'}
+            display="block"
+          />
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              gap: '0.25rrem',
+            }}
+          >
+            <ProfileTitle name="고명한" className="lbl-lg"></ProfileTitle>
+            <p className="para-sm" style={{ color: 'var(--gray-600)' }}>
+              작성글10
             </p>
           </div>
         </div>
