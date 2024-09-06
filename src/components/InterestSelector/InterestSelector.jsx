33f3af183671cbba8e90d73ffd1a7e573e4d89cc
dongho <ldd0702@naver.com>
2024-09-06 23:34:26 +0900

    Feature 관심분야 선택 페이지 렌더링 작업
    - 관심분야선택 컴포넌트 추가
    - 관심분야선택 컴포넌트 스타일링
    - 관심분야카드리스트 수파베이스 데이터 렌더링
    - 관심분야 페이지 렌더링

---
+++ src/components/InterestSelector/InterestSelector.jsx
@@ -0,0 +1,80 @@
+import { useState, useRef, useEffect } from 'react';
+import Button from '../Button/Button';
+import S from './InterestSelector.module.css';
+import { useSupabase } from '@/api/DataService';
+
+function InterestSelector({ onSelectInterest }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const modalRef = useRef(null);
+  const { interest } = useSupabase();
+
+  const handleOpenSelector = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      handleClose();
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('mousedown', handleOutsideClick);
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.removeEventListener('mousedown', handleOutsideClick);
+      document.body.style.overflow = 'unset';
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+      document.body.style.overflow = 'unset';
+    };
+  }, [isOpen]);
+
+  const handleInterestClick = (interest) => {
+    onSelectInterest(interest);
+    handleClose();
+  };
+
+  return (
+    <div>
+      <Button color="white" onClick={handleOpenSelector}>
+        <span className="i_target"></span>관심분야 선택하기
+      </Button>
+      {isOpen && (
+        <div className={`${S.backdrop} ${S.open}`}>
+          <div
+            className={`${S.container} ${isOpen ? S.show : ''}`}
+            ref={modalRef}
+          >
+            <div className={S.header}>
+              <h2>카테고리 선택</h2>
+              <button onClick={handleClose} className={S.closeButton}>
+                <span className="i_close"></span>
+              </button>
+            </div>
+            <div className={S.interestList}>
+              {interest.map((interest) => (
+                <button
+                  key={interest}
+                  className={S.interestButton}
+                  onClick={() => handleInterestClick(interest)}
+                >
+                  {interest.name}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default InterestSelector;
