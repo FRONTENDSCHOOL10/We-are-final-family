37ef78cce7d0810a36cefe4560eb5a5d0d201492
dongho <ldd0702@naver.com>
2024-09-06 22:03:38 +0900

    수파베이스 훅 수정

--- src/hooks/useSupabase.jsx
+++ src/hooks/useSupabase.jsx
@@ -3,6 +3,8 @@ import { supabase } from '../api/supabase';
 
 export const useSupabase = () => {
   const [users, setUsers] = useState([]);
+  const [interest, setinterest] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
 
   const getUsers = async () => {
     const { data, error } = await supabase.from('users').select('*');
@@ -13,12 +15,34 @@ export const useSupabase = () => {
   const createUser = async (userData) => {
     const { data, error } = await supabase.from('users').insert([userData]);
     if (error) console.error('Error creating user:', error);
-    else getUsers(data); // Refresh the user list
+    else getUsers(); // Refresh the user list
+  };
+
+  const getinterest = async () => {
+    const { data, error } = await supabase.from('interest').select('*');
+    if (error) console.error('Error fetching interest:', error);
+    else setinterest(data);
+  };
+
+  const getSubCategory = async () => {
+    const { data, error } = await supabase.from('sub_category').select(`
+      id,
+      name,
+      Category:category_id (name)
+    `);
+    if (error) console.error('Error fetching sub_category:', error);
+    else setSubCategory(data);
   };
 
   useEffect(() => {
     getUsers();
+    getinterest();
+    getSubCategory();
   }, []);
 
-  return { users, createUser };
+  return {
+    users,
+    interest,
+    subCategory,
+  };
 };
