869e7ca7b18095e630ceba1cfd33aa862bdbbbe9
ttining <ttining.real@gmail.com>
2024-09-09 16:46:15 +0900

    Feature: Helmet(meta), Suspense, Error, Fallback 추가
    - Router 패스 별 텍스트 추가
    - Error 페이지 추가
    - Fallback 페이지 추가
    - Board 하위 페이지 추가 (상세, 작성)
    - RootLayout 헬멧 + meta 추가

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,12 +1,46 @@
 import S from './RootLayout.module.css';
-import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
-import Spinner from '@/components/App/Spinner';
+import { Outlet } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import { Fallback } from '@/pages';
 
 function RootLayout() {
   return (
     <div className={S.root}>
-      <Suspense fallback={<Spinner />}>
+      <Helmet>
+        <title>파티구함</title>
+        <meta
+          name="description"
+          content="스터디, 프로젝트, 자기 계발을 위한 다양한 파티를 찾아보세요. 관심사에 맞는 파티를 구하고 함께 성장할 수 있는 자유로운 스터디 커뮤니티 플랫폼입니다. 누구나 쉽게 참여할 수 있습니다."
+        />
+        <meta
+          property="og:title"
+          content="파티구함 - 자유로운 스터디 커뮤니티 플랫폼"
+        />
+        <meta
+          property="twitter:title"
+          content="파티구함 - 자유로운 스터디 커뮤니티 플랫폼"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://github.com/FRONTENDSCHOOL10/we-are-final-family"
+        />
+        <meta
+          property="og:description"
+          content="스터디, 프로젝트, 자기 계발을 위한 다양한 파티를 찾아보세요. 관심사에 맞는 파티를 구하고 함께 성장할 수 있는 자유로운 스터디 커뮤니티 플랫폼입니다. 누구나 쉽게 참여할 수 있습니다."
+        />
+        <meta
+          property="og:image"
+          content="https://github.com/FRONTENDSCHOOL10/we-are-final-family/blob/Develop/public/logo_192.png"
+        />
+        <meta property="og:site_name" content="파티구함" />
+        <meta
+          property="og:site_author"
+          content="우리는 파이널 7ㅏ조쿠(we-are-final-family)"
+        />
+      </Helmet>
+      <Suspense fallback={<Fallback />}>
         <Outlet />
       </Suspense>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
+import { lazy } from 'react';
+
+// Intro (인트로)
 export { default as Intro } from './Intro/Intro';
+
+// Sign In/Up (로그인 / 회원가입)
 export { default as Login } from './Login/Login';
 export { default as Interest } from './Register/Interest';
 export { default as Register } from './Register/Register';
+
+// Search (검색)
 export { default as Search } from './Search/Search';
-export { default as Home } from './Home/Home';
-export { default as HomeDetail } from './Home/HomeDetail';
+
+// Home (파티모집)
+export const Home = lazy(() => import('./Home/Home'));
+export const HomeDetail = lazy(() => import('./Home/HomeDetail'));
 export { default as HomeWrite } from './Home/HomeWrite';
-export { default as Board } from './Board/Board';
+
+// Board (게시판)
+export const Board = lazy(() => import('./Board/Board'));
+export const BoardDetail = lazy(() => import('./Board/BoardDetail'));
+export { default as BoardWrite } from './Board/BoardWrite';
+
+// Chat (채팅)
 export { default as Chat } from './Chat/Chat';
-export { default as ChatRoom } from './Chat/ChatRoom';
+export const ChatRoom = lazy(() => import('./Chat/ChatRoom'));
+
+// Profile (프로필)
 export { default as Profile } from './Profile/Profile';
 export { default as ProfileView } from './Profile/ProfileView';
 export { default as ProfileEdit } from './Profile/ProfileEdit';
+
+// 기타
+export { default as Error } from './Error';
+export { default as Fallback } from './Fallback';
