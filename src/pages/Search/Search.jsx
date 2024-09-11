98ec440f570a09632e9aa506a4d2ec8bcf96cfd4
dongho <ldd0702@naver.com>
2024-09-12 08:53:59 +0900

    최근검색어 메세지 제거

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -220,7 +220,7 @@ function Search() {
                 </span>
               ))
             ) : (
-              <p>최근 검색어가 없습니다.</p>
+              <p></p>
             )}
           </div>
         </section>
