488a97b25e61e4e5e4588bfa351d3225773753c8
dongho <ldd0702@naver.com>
2024-09-12 11:33:05 +0900

    feature 검색기능구현

--- src/components/ListFilterButtons/data/ListFilterButtonsData.js
+++ src/components/ListFilterButtons/data/ListFilterButtonsData.js
@@ -4,3 +4,9 @@ export const filterOptions = [
   { key: 'anyone', label: '누구나' },
   { key: '10', label: '10대' },
 ];
+
+export const searchFilterOptions = [
+  { key: 'all', label: '전체' },
+  { key: 'party', label: '파티모집' },
+  { key: 'board', label: '게시판' },
+];
