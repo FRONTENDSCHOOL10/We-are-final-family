5b7f9d8ec3f6c46e72ce663916bee9a7c6ae7a88
ttining <ttining.real@gmail.com>
2024-09-23 23:36:18 +0900

    Style 다크 모드 추가
    - 인트로
    - 홈 > 필터 모달
    - 게시판 > 작성하기
    - 회원가입/프로필 > 관심분야 선택 필터 모달

--- src/pages/Fallback.jsx
+++ src/pages/Fallback.jsx
@@ -7,14 +7,14 @@ function Fallback() {
         flex: '1',
         display: 'flex',
         flexFlow: 'column nowrap',
-        gap: '3rem',
         alignItems: 'center',
         justifyContent: 'center',
+        gap: '3rem',
         height: '100vh',
         color: 'var(--gray-700)',
       }}
     >
-      <LogoAnimation width="7rem" height="7rem" />
+      <LogoAnimation width="5rem" height="5rem" />
       <p className="para-md">페이지를 불러오고 있습니다.</p>
     </div>
   );
